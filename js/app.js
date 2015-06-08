8594c9061ed081145333b5ef04e50a9b61ef47e9
Chris Hamm <chris.m.hamm@gmail.com>
2015-06-08 12:28:04 -0400

    check/uncheck toggle work

--- js/app.js
+++ js/app.js
@@ -1,49 +1,18 @@
 $(document).ready(function() {
 
-function playHadouken() {
-    $('#hadouken-sound')[0].volume=1;
-    $('#hadouken-sound')[0].load();
-    $('#hadouken-sound')[0].play();
-}
+//Check/Uncheck Toggle
+    $('.item-active').click(function() {
+        $(this).removeClass('item-active').addClass('item-checked');
+});
 
-    $('.ryu').mouseenter(function() {
-        $('.ryu-still').hide();
-        $('.ryu-ready').show();
-    });
+    $('.item-checked').click(function() {
+        $(this).removeClass('item-checked').addClass('item-active');
+});
 
-    $('.ryu').mouseleave(function() {
-        $('.ryu-ready').hide();
-        $('.ryu-still').show();
-    });
 
-    $('.ryu').mousedown(function() {
-        playHadouken();
-        $('.ryu-ready').hide();
-        $('.ryu-throwing').show();
-        $('.hadouken').finish().show().animate(
-            {'left': '1072px'},
-            300,
-            function() {
-                $(this).hide()
-                $(this).css('left', '572px');
-            }
-            );
-    });
 
-    $('.ryu').mouseup(function() {
-        $('.ryu-throwing').hide();
-        $('.ryu-ready').show();
-    });
 
-    $(document).keydown(function(e) {
-        if(e.which == 88){
-            $('.ryu-ready').hide();
-            $('.ryu-still').hide();
-            $('.ryu-pose').show();
-        }
-    });
-    $(document).keyup(function(e) {
-            $('.ryu-still').show();
-            $('.ryu-pose').hide();
-    });
+
+
+
 });
