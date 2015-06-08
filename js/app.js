e584e1304089caaf014549f7daf0ce1c277ba59e
Chris Hamm <chris.m.hamm@gmail.com>
2015-06-08 14:51:44 -0400

    new item add

--- js/app.js
+++ js/app.js
@@ -2,17 +2,18 @@ $(document).ready(function() {
 
 //Check/Uncheck Toggle
     $('.item-active').click(function() {
-        $(this).removeClass('item-active').addClass('item-checked');
+        $(this).toggleClass('item-active item-checked');
 });
 
     $('.item-checked').click(function() {
-        $(this).removeClass('item-checked').addClass('item-active');
+        $(this).toggleClass('item-checked item-active');
 });
 
+   $('.add-items').click(function() {
+        $('.list-items:first-child').prepend('<li class="item"><button class="item-active">≡ Test Item</button><button class="delete-item">X</button></li>');
+});
 
-
-
-
-
+ $('.delete-item').click(function() {
+     $(this)
 
 });
